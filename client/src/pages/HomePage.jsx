f2eb210ad35a82a31f7c1126f7a553eef66d3f3d
Laura <lauraalspaugh2002@icloud.com>
2024-05-14 12:48:58 -0400

    outline

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -4,20 +4,13 @@ export default function HomePage() {
   const [count, setCount] = useState(0)
 
   return (
+    <div>
     <div className="home-page">
-      <div className="container my-3">
-        <div className="row">
-          <div className="col-4">
-            <div className="card">
-              <div className="card-body">
-                <button className="btn btn-success my-1" onClick={() => setCount((count) => count + 1)}>
-                  count is {count}
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <h1 className="p-4 text-center dream-heading">Where Dreams Meet Reality.</h1>
+    </div>
+    <div className="home-page">
+      <img className="img-main" alt="main image" src="https://images.unsplash.com/photo-1715390321213-c8d88b3e024a?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3Nnx8fGVufDB8fHx8fA%3D%3D"></img>
+    </div>
     </div>
   )
 }
